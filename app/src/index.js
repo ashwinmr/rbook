63ad561e6b4ea46d0ba752b44225bec1c4be07c0
ashwinmr <iashwinrao@gmail.com>
2019-12-09 21:48:02 +0530

    add page number support (current page not working)

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,11 @@ const path = require('path')
 const url = require('url')
 const ePub = require('epubjs').default
 
+// Update page display
+function updatePage(current, total) {
+    document.getElementById('page_number').textContent = current + '/' + total
+}
+
 // Create object to handle file
 class FileClass {
 
@@ -41,31 +46,45 @@ class BookClass {
     constructor() {
         this.data = undefined
         this.rendition = undefined
+        this.pages = undefined
+    }
+
+    // To-do: find a way to get current page
+    get currentPage() {
+        if (this.rendition !== undefined) {
+            return 0
+        } else {
+            return 0
+        }
     }
 
     load(filePath) {
         this.data = ePub(filePath)
-        this.display()
+        this.data.ready.then(() => {
+            this.pages = this.data.pageList.lastPage
+            this.display()
+        })
     }
 
     display() {
         this.rendition = this.data.renderTo("book_cont");
         var displayed = this.rendition.display();
+        updatePage(this.currentPage, this.pages)
     }
 
     nextPage() {
         if (this.rendition !== undefined) {
             this.rendition.next()
+            updatePage(this.currentPage, this.pages)
         }
     }
 
     previousPage() {
         if (this.rendition !== undefined) {
             this.rendition.prev()
+            updatePage(this.currentPage, this.pages)
         }
     }
-
-
 }
 var Book = new BookClass
 
